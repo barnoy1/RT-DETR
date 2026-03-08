52cc8f3b456eae380b00adf0195494904e91e6e3
ronbar <ronbar@rafael.co.il>
2026-03-08 15:26:56 +0200

    non intrusive generic adapter

--- rtdetrv2_pytorch/nn_wrapper/__init__.py
+++
@@ -1,7 +0,0 @@
-from .adapter import RTDETRv2ModelBuilder, create_model_builder, create_wrapper_components
-
-__all__ = [
-    "RTDETRv2ModelBuilder",
-    "create_model_builder",
-    "create_wrapper_components",
-]
